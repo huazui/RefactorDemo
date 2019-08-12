e814d8d9a3b2692ae60fdf1e8988600f11cf3688
huazui <2509155771@qq.com>
2019-08-12 21:56:55 +0800

    Add files via upload
    将所有的name提取出来并且改前面没有考虑好的小细节。

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -6,104 +6,119 @@ public class GildedRose {
         this.items = items;
     }
 
-    public void updateAged(Item item){
-
-        if (item.quality < 50&&item.sellIn <0) {
-            item.quality = item.quality + 2;
+    public void updateAged(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
         }
-        else if(item.quality <50)
-            item.quality += 1;
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0 && item.quality < 50)
+            item.quality = item.quality + 1;
     }
-    public void updateBackstage(Item item){
-        if(item.sellIn < 0) {
+
+    public void updateBackstage(Item item) {
+        if (item.sellIn < 1) {
             item.quality = 0;
             return;
         }
-        if(item.quality < 50){
-            if(item.sellIn < 6){
+
+        if (item.quality < 50) {
+            if(item.sellIn < 6)
                 item.quality += 3;
+             else if (item.sellIn < 11)
+               item.quality += 2;
+               else item.quality++;
             }
-            else if(item.sellIn <11){
-                item.quality += 2;
-            }
-            else item.quality += 1;
-        }
-        }
+        item.sellIn = item.sellIn - 1;
+    }
 
+public void updateSulfuras(Item item){
+}
 
+public void updateOthers(Item item){
+    if (item.quality > 0) {
+            item.quality = item.quality - 1;
+    }
 
- public void updateItemQuality(Item item){
-     if (!item.name.equals("Aged Brie")
-             && !item.name.equals("Backstage passes to a TAFKAL80ETC concert"))   {
-         if (item.quality > 0) {
-             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                 item.quality = item.quality - 1;
-             }
-         }
-     }
-     else {
-         if (item.quality < 50) {
-             item.quality = item.quality + 1;
+    item.sellIn = item.sellIn - 1;
 
-             if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                 if (item.sellIn < 11) {
-                     if (item.quality < 50) {
-                         item.quality = item.quality + 1;
-                     }
-                 }
-
-                 if (item.sellIn < 6) {
-                     if (item.quality < 50) {
-                         item.quality = item.quality + 1;
-                     }
-                 }
-             }
-         }
-     }
-     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-         item.sellIn = item.sellIn - 1;
-     }
-
-     if (item.sellIn < 0) {
-         if (!item.name.equals("Aged Brie")) {
-             if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                 //D
-                 if (item.quality > 0) {
-                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                         item.quality = item.quality - 1;
-                     }
-                 }
-             }
-
-             else {
-                 item.quality = item.quality - item.quality;
-             }
-         }
-
-         else {
-             if (item.quality < 50) {
-                 item.quality = item.quality + 1;
-             }
-         }
-     }
-
- }
+    if (item.quality > 0&&item.sellIn<0) {
+            item.quality = item.quality - 1;
+    }
+}
+// public void updateItemQuality(Item item){
+//     if (!item.name.equals("Aged Brie")
+//             && !item.name.equals("Backstage passes to a TAFKAL80ETC concert"))   {
+//         if (item.quality > 0) {
+//             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+//                 item.quality = item.quality - 1;
+//             }
+//         }
+//     }
+//     else {
+//         if (item.quality < 50) {
+//             item.quality = item.quality + 1;
+//
+//             if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+//                 if (item.sellIn < 11) {
+//                     if (item.quality < 50) {
+//                         item.quality = item.quality + 1;
+//                     }
+//                 }
+//
+//                 if (item.sellIn < 6) {
+//                     if (item.quality < 50) {
+//                         item.quality = item.quality + 1;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+//         item.sellIn = item.sellIn - 1;
+//     }
+//
+//     if (item.sellIn < 0) {
+//         if (!item.name.equals("Aged Brie")) {
+//             if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+//                 //D
+//                 if (item.quality > 0) {
+//                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+//                         item.quality = item.quality - 1;
+//                     }
+//                 }
+//             }
+//
+//             else {
+//                 item.quality = item.quality - item.quality;
+//             }
+//         }
+//
+//         else {
+//             if (item.quality < 50) {
+//                 item.quality = item.quality + 1;
+//             }
+//         }
+//     }
+//
+// }
 
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-
-            if(items[i].name == "Aged Brie") {
-                updateAged(items[i]);
-                break;
-            }
-            if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-            {
+          if(items[i].name == "Aged Brie") {
+              updateAged(items[i]);
+              return;
+          }
+            if(items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
                 updateBackstage(items[i]);
-                break;
+                return;
+            }
+            if(items[i].name == "Sulfuras, Hand of Ragnaros") {
+               updateSulfuras(items[i]);
+                return;
             }
 
-            updateItemQuality(items[i]);
+            updateOthers(items[i]);
 
 
         }
